6c7dbe4c11a15344bbfd4200d36b2b6ca1ba8f98
Harald Sagar Vohringer <harald.voeh@gmail.com>
2018-12-20 18:25:30 +0100

    first index.html

--- loops/application.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-	names = ['Mark', 'Bill', 'harald', 'King Kong']
-	return render_template("index.html", names=names)
